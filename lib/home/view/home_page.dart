315131a9df620f98b885e5997e49cd305c07d653
Ammar <ammar.alfawal@gmx.de>
2025-02-03 10:24:41 +0100

    Change

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:iss_mir_egal/home/models/meal.dart';
 import 'package:iss_mir_egal/home/view/meals_overview_page.dart';
 import 'package:iss_mir_egal/home/view/profile_page.dart';
-import 'package:iss_mir_egal/home/widgets/meal_card.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({required this.meals, super.key});
